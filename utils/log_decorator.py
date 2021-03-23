04148c6143ec5c8611af10a64f73462c89879d5c
Raider992 <19stargazer92@gmail.com>
2021-03-23 03:16:35 +0300

    Дз-7 готово. По логике всё работает, файлы клиента и сервера запускаются, авторизация проходит. Есть одна проблема, её так и не решил. Почему-то после первого удачного запуска, когда пытаюсь запустить client.py с другим портом, всплывает ошибка в строке 79 файла client.py. "UnboundLocalError: local variable 'data' referenced before assignment" В первый раз работает, дальше это. Голову сломал, так и не могу понять, в чём дело.

--- utils/log_decorator.py
+++ utils/log_decorator.py
@@ -1,30 +1,20 @@
 from functools import wraps
-import logging
 import inspect
+import logging
+import utils.log.log_decorator_config
 
 
-def config_logger():
-    logger = logging.getLogger('func_tracing_class')
-    file_handler = logging.FileHandler('utils/log/func_call.log', encoding='utf-8')
-    _format = logging.Formatter('%(message)s : %(asctime)s - %(func_name)s - %(func_args)s из %(parent_func)s')
-    file_handler.setFormatter(_format)
-    logger.addHandler(file_handler)
-    logger.setLevel(logging.DEBUG)
-
-
-class Trace():
-    def __init__(self):
-        config_logger()
+class Trace:
 
     def __call__(self, func):
         @wraps(func)
         def decorated(*args, **kwargs):
-            res = func(*args, **kwargs)
             logger = logging.getLogger('func_tracing_class')
             stack = inspect.stack()
             parent_func = stack[1].function
             arg = f'{args} {kwargs}'
-            logger.debug('Функция вызвана', extra={'func_name': func.__name__, 'func_args': arg, 'parent_func': parent_func})
-            return res
+            logger.debug('Функция вызвана',
+                         extra={'func_name': func.__name__, 'func_args': arg, 'parent_func': parent_func})
+            return func(*args, **kwargs)
 
         return decorated
